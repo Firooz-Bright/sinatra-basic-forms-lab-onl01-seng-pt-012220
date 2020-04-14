7cf42f31aa85a68304265a430885b2a1e4eb0c38
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-04-14 10:27:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  
+  get '/'  do 
+    
+    erb :create_puppy
+  end 
+  
+  
+  post '/'  do 
+    
+    erb :display_puppy
+    
+  end 
+  
+  
+  post'/' do 
+    
+    erb :index
+    
+  end 
+  
+  
+ 
 end
